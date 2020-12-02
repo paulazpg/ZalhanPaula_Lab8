3ae8e30119f766bfa794cbc3dcb3578c4e927276
Paula - Georgiana ZALHAN <Paula.ZALHAN@office.ubbcluj.ro>
2020-12-02 02:00:19 +0200

    Am modificat BookCategory

--- Models/BookCategory.cs
+++ Models/BookCategory.cs
@@ -2,10 +2,13 @@
 {
     public class BookCategory
     {
+        //fields in table
         public int ID { get; set; }
-        public int BookID { get; set; }
-        public Book Book { get; set; }
+        public int BookID { get; set; }      
         public int CategoryID { get; set; }
+
+        // navigation properties
+        public Book Book { get; set; }
         public Category Category { get; set; }
     }
 }
